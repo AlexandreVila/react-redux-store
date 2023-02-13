ee5adaa9138b0d0c35803c8d4ceba3343d950fdc
Alexandre Vila <alexandrevn@alu.ufc.br>
2023-02-13 14:01:25 -0300

    criacao da pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { MdAddShoppingCart } from 'react-icons/md'
 
-export default function Cart() {
-    return <h1> 
-        home
-    </h1>;
+import { ProductList } from './styles'
+
+export default function Home() {
+    return (
+        <ProductList>
+            <li>
+                <div> img </div>
+                <strong>Tenis</strong>
+                <span> R$12,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color= "#fff" /> 3
+                    </div>
+                    <span> Adicionar ao carrinho </span>
+                </button>
+            </li>
+            <li>
+                <div> img </div>
+                <strong>Tenis</strong>
+                <span> R$12,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color= "#fff" /> 3
+                    </div>
+                    <span> Adicionar ao carrinho </span>
+                </button>
+            </li>
+            <li>
+                <div> img </div>
+                <strong>Tenis</strong>
+                <span> R$12,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color= "#fff" /> 3
+                    </div>
+                    <span> Adicionar ao carrinho </span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
